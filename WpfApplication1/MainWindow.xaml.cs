aa7996ef0bef6d2cb493d7a2e1238faddf11a38e
Lev <MulTiWolf01@gmail.com>
2015-04-26 01:41:16 +0500

    Системы счисления
    Системы счисления

---
+++ WpfApplication1/MainWindow.xaml.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WpfApplication1
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Button1_Click(object sender, RoutedEventArgs e)
+        {
+            string text = TextBox1.Text;
+            TextBox2.Text = Convert.ToString(Convert.ToInt32(text), 2);
+        }
+    }
+}
